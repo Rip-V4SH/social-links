86d56c6ccbd40e3722e1d756fdb2b5f67e090d42
Igor Almeida <tioban.alm@gmail.com>
2024-05-24 17:04:07 +0000

    feat: 'Home.jsx' remade.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import { HomeContainer } from "./Home.styles"
+
+export default function Home() {
+  return (
+    <HomeContainer>
+        <img src="" alt="" />
+        <h1>Igor Fernandes Almeida</h1>
+        <h3>São Paulo, Brasil</h3>
+        <h3>"exsisting"</h3>
+        <button>GitHub</button>
+        <button>LinkedIn</button>
+        <button>Instagram</button>
+    </HomeContainer>
+  )
+}
